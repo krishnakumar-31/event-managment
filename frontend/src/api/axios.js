cfe331dfb5dc89fd45a0e2b1b01ad9c400b39de7
krishnakumar-31 <krishnasgk003@gmail.com>
2026-07-09 19:43:57 +0530

    Change API base URL to production server

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://127.0.0.1:5000", // VERY IMPORTANT
+  baseURL: "https://event-managment-vniv.onrender.com",
 });
 
 export default API;
